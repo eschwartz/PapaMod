21d3f7879187c0d2a3d094705d7d793e377ec4cb
Edan Schwartz <edanschwartz@gmail.com>
2020-12-27 14:29:17 -0600

    Llama on any right click

--- src/main/java/com/example/examplemod/CustomSword.java
+++ src/main/java/com/example/examplemod/CustomSword.java
@@ -6,8 +6,10 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.passive.horse.LlamaEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.*;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Direction;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.world.World;
@@ -20,11 +22,40 @@ public class CustomSword extends SwordItem {
         super(ItemTier.DIAMOND, 3, -3.0F, (new Item.Properties()).group(ItemGroup.COMBAT));
     }
 
-    
+    /**
+     * Called to trigger the item's "innate" right click behavior. To handle when this item is used on a Block, see
+     * {@link #onItemUse}.
+     *
+     * @param world
+     * @param player
+     * @param hand
+     */
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World world, PlayerEntity player, Hand hand) {
+        player.sendStatusMessage(
+                new StringTextComponent("Happy Llamukkah"),
+                true
+        );
+
+        BlockPos playerPos = player.getPosition();
+        Direction facing = player.getHorizontalFacing();
+
+        Entity llama = new LlamaEntity(EntityType.LLAMA, world);
+        llama.setPosition(
+                playerPos.getX() + facing.getXOffset() * 4,
+                playerPos.getY() + facing.getYOffset() * 4,
+                playerPos.getZ() + 1
+        );
+
+        world.addEntity(llama);
+
+        return super.onItemRightClick(world, player, hand);
+    }
 
     /**
      * Called on right-click, when pointed at a block or something
      */
+    /*
     @Override
     public ActionResultType onItemUse(ItemUseContext context) {
         PlayerEntity player = context.getPlayer();
@@ -50,7 +81,7 @@ public class CustomSword extends SwordItem {
         world.addEntity(llama);
 
         return super.onItemUse(context);
-    }
+    }*/
 
     @Override
     public boolean onLeftClickEntity(ItemStack stack, PlayerEntity player, Entity entity) {
